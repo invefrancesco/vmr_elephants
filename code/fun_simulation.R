364c7aaff346a58942766c91ca141b12325299a3
invefrancesco <invernizzi.francesco.2000@gmail.com>
2026-05-08 16:58:10 +0100

    Refactor simulation function to streamline parameter handling and improve clarity; remove auxiliary functions.

--- code/fun_simulation.R
+++ code/fun_simulation.R
@@ -6,90 +6,51 @@
 pacman::p_load(
   circular
 )
-
-# --------------------------------------------------------------------------- #
-#   AUXILIARY FUNCTIONS
-# --------------------------------------------------------------------------- #
-# --- get_tau ---
+#' --------------------------------------------------------------------------- #
+#'   MAIN SIMULATION FUNCTION
+#' --------------------------------------------------------------------------- #
+#' --- simulate_burst ---
+#' Generate a single trajectory from a Circular MAR model
 #'
-#' @description
-#' Gives a matrix where each cell contains the circular deviation at lag h for
-#' component k.
+#' @param n number of observations (after burn-in)
+#' @param mod list containing model parameters:
+#'   - K: number of components
+#'   - h: autoregressive order
+#'   - mu: baseline mean directions (length K)
+#'   - kappa: baseline concentrations (length K)
+#'   - arcoef: AR coefficients (matrix h x K)
+#'   - prob: mixing probabilities (length K)
+#' @param burn length of burn-in period to reach stationarity
 #'
-#' @param t the current time
-#' @param K the number of mixture components
-#' @param h the autoregressive order
-#' @param mu the non-conditional mean
-
-get_tau <- function(t, K, h, x, mu) {
-  tau <- matrix(ncol = K, nrow = h)
-  for (g in 1:h) {
-    ifelse(
-      t > g,
-      tau[g, ] <- sin(x[t - g] - mu),
-      tau[g, ] <- 0
-    )
-  }
-  tau
-}
-
-# --- get_upsilon ---
-#'
-#' @description
-#' Gives a matrix where each cell contains the circular deviation at lag h for
-#' component k scaled by concentration kappa.
-#'
-#' @param t the current time
-#' @param K the number of mixture components
-#' @param h the autoregressive order
-#' @param mu the non-conditional mean
-#'
-get_upsilon <- function(t, K, h, x, mu, kappa) {
-  upsilon <- matrix(ncol = K, nrow = h)
-  for (g in 1:h) {
-    ifelse(
-      t > g,
-      upsilon[g, ] <- sin(x[t - g] - mu) / kappa,
-      upsilon[g, ] <- 0
-    )
-  }
-  upsilon
-}
-
-# --------------------------------------------------------------------------- #
-#   MAIN SIMULATION FUNCTION
-# --------------------------------------------------------------------------- #
-# --- simulate_burst ---
-#'
-#'
-simulate_burst <- function(n, prob, kappa, mu, arcoef) {
-  K <- length(prob) # Number of mixture components
-  h <- nrow(arcoef) # Autoregressive order
-
+#' @return a vector of circular observations
+sim.burst <- function(n, mod, burn = 500) {
+  n <- n + burn
   # --- Init time varying parameters ---
-  kappa.t <- matrix(ncol = K, nrow = n)
-  kappa.t[1, ] <- kappa
+  kappa.t <- matrix(ncol = mod$K, nrow = n)
+  kappa.t[1:mod$h, ] <- mod$kappa
 
-  mu.t <- matrix(ncol = K, nrow = n)
-  mu.t[1, ] <- mu
+  mu.t <- matrix(ncol = mod$K, nrow = n)
+  mu.t[1:mod$h, ] <- mod$mu
 
   # --- Latent variable z ---
-  z <- sample(1:K, n, replace = TRUE, prob)
+  z <- sample(1:mod$K, n, replace = TRUE, mod$prob)
 
-  # --- Initial sampling for the first time step (t=1) ---
+  # --- Initial sampling for the first h time steps ---
   x <- numeric(n)
-  x[1] <- circular::rvonmises(1, mu = mu[z[1]], kappa = kappa[z[1]])
-
-  # --- Sampling for the time steps 2:n ---
-  for (t in 2:n) {
-    tau <- get_tau(t, K, h, x, mu)
-    kappa.t[t, ] <- sqrt(kappa^2 + colSums(arcoef * tau)^2)
-
-    upsilon <- get_upsilon(t, K, h, x, mu, kappa)
-    mu.t[t, ] <- mu + atan(colSums(arcoef * upsilon))
-
-    x[t] <- circular::rvonmises(1, mu = mu.t[t, z[t]], kappa = kappa.t[t, z[t]])
+  for (i in 1:mod$h) {
+    x[i] <- circular::rvonmises(1, mu = mod$mu[z[i]], kappa = mod$kappa[z[i]])
   }
 
+  # --- Sampling for the time steps h:n ---
+  for (t in (mod$h + 1):n) {
+    for (k in 1:mod$K) {
+      foo <- x[(t - 1):(t - h)]
+      foo <- as.numeric(mod$arcoef[, k] %*% sin(foo - mod$mu[k]))
+      kappa.t[t, k] <- sqrt(mod$kappa[k]^2 + foo^2)
+      mu.t[t, k] <- mod$mu[k] + atan(foo / mod$kappa[k])
+    }
+    x[t] <- circular::rvonmises(1, mu = mu.t[t, z[t]], kappa = kappa.t[t, z[t]])
+  }
+  x <- x[(burn + 1):length(x)]
   x
 }
